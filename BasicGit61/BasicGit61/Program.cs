58c5f60c37c3e13db061b8e25015f4088729ccec
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2021-12-16 14:01:38 +0700

    Create new project

---
+++ BasicGit61/BasicGit61/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicGit61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
